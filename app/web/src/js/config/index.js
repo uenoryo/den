9e727f40bb81e1b48f4ff8903a948ed21462d6e1
uenoryo <r.r.ag0611@outlook.jp>
2018-06-15 23:11:10 +0900

    Add config file.

---
+++ app/web/src/js/config/index.js
@@ -0,0 +1,10 @@
+import Constants from '@/constants/constants'
+
+export default {
+  EntryPlayers {
+    0: Constants.PlayerTypePlayer,
+    1: Constants.PlayerTypePlayer,
+    2: Constants.PlayerTypePlayer,
+    3: Constants.PlayerTypePlayer,
+  },
+}
