1b9deb3f7c001d6a4d5d4d0f63d29161094eb607
uenoryo <r.r.ag0611@outlook.jp>
2018-06-15 23:10:43 +0900

    Add Game view.

---
+++ app/web/src/js/components/Game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='game'>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import Player from '@/models/Player'
+
+export default {
+  name: 'Game',
+  data () {
+    return {
+      message: 'Play',
+    }
+  },
+  mounted () {
+    let p = new Player("test")
+    console.log(p)
+  }
+}
+</script>
