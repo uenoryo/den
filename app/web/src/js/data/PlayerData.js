019b92eab0aa19bd31cd1efc4d3691419b64bfcc
uenoryo <r.r.ag0611@outlook.jp>
2018-06-15 23:25:10 +0900

    Add PlayerData

---
+++ app/web/src/js/data/PlayerData.js
@@ -0,0 +1,24 @@
+import Constants from '@/constants'
+
+export default class PlayerData {
+  constructor(id, type) {
+    if (
+      id !== Constants.Player1ID &&
+      id !== Constants.Player2ID &&
+      id !== Constants.Player3ID &&
+      id !== Constants.Player4ID
+    ) {
+      throw new Error("Invalid Player ID.")
+    }
+
+    if (
+      type !== Constants.PlayerTypePlayer &&
+      type !== Constants.PlayerTypeComputer
+    ) {
+      throw new Error("Invalid Player Type.")
+    }
+
+    this.id = id
+    this.type = type
+  }
+}
