2889a7463388cce0db54acc43d80fb0795eb8c7b
uenoryo <r.r.ag0611@outlook.jp>
2018-06-16 00:30:06 +0900

    Add CardData.

---
+++ app/web/src/js/data/CardData.js
@@ -0,0 +1,27 @@
+import Constants from '@/constants'
+
+export default class CardData {
+  constructor(mark, num) {
+    if (
+      mark !== Constants.CardMarkClub &&
+      mark !== Constants.CardMarkDiamond &&
+      mark !== Constants.CardMarkHeart &&
+      mark !== Constants.CardMarkSpade &&
+      mark !== Constants.CardMarkJoker
+    ) {
+      throw new Error(`Invalid Card Mark[${mark}].`)
+    }
+
+    num = parseInt(num)
+    if (num < Constants.CardMinNum || num > Constants.CardMaxNum) {
+      throw new Error(`Invalid Card Num[${num}]`)
+    }
+
+    if (mark === Constants.CardMarkJoker && num !== Constants.CardJokerNum) {
+      throw new Error(`Invalid Card Joker Num[${num}]`)
+    }
+
+    this.mark = mark
+    this.num = num
+  }
+}
