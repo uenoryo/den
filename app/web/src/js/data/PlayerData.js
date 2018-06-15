9f44b45a0848810850e7b57aaea080a3738e0639
uenoryo <r.r.ag0611@outlook.jp>
2018-06-16 00:08:54 +0900

    Cast player id and type.

--- app/web/src/js/data/PlayerData.js
+++ app/web/src/js/data/PlayerData.js
@@ -2,20 +2,22 @@ import Constants from '@/constants'
 
 export default class PlayerData {
   constructor(id, type) {
+    id = parseInt(id)
     if (
       id !== Constants.Player1ID &&
       id !== Constants.Player2ID &&
       id !== Constants.Player3ID &&
       id !== Constants.Player4ID
     ) {
-      throw new Error("Invalid Player ID.")
+      throw new Error(`Invalid Player ID[${id}].`)
     }
 
+    type = parseInt(type)
     if (
       type !== Constants.PlayerTypePlayer &&
       type !== Constants.PlayerTypeComputer
     ) {
-      throw new Error("Invalid Player Type.")
+      throw new Error('Invalid Player Type.')
     }
 
     this.id = id
