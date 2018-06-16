9b2bfe5f6f266f7cc69bfcfb8127a3f571e51ec5
uenoryo <r.r.ag0611@outlook.jp>
2018-06-16 23:35:41 +0900

    DeckData 追加.

---
+++ app/web/src/js/data/DeckData.js
@@ -0,0 +1,21 @@
+import Constants from '../constants'
+
+export default class DeckData {
+  constructor(cards) {
+    this.cards(cards)
+  }
+
+  cards(cards) {
+    if (Array.isArray(cards) === false) {
+      throw new Error(`Invalid Cards [${cards}].`)
+    }
+
+    for (let idx in cards) {
+      if (cards[idx].constructor.name !== 'CardData') {
+        throw new Error(`Invalid Card [${cards[idx].constructor.name}]`)
+      }
+    }
+
+    this.Cards = cards
+  }
+}
