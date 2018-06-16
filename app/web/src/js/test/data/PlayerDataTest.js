7158b3d2b03f59046a22f5f2978824613049b29c
uenoryo <r.r.ag0611@outlook.jp>
2018-06-16 23:48:00 +0900

    テスト更新.

--- app/web/src/js/test/data/PlayerDataTest.js
+++ app/web/src/js/test/data/PlayerDataTest.js
@@ -1,18 +1,42 @@
 import assert from 'assert'
 import PlayerData from '../../data/PlayerData'
 
-describe('PlayerData', function () {
-  it('正しく初期化できる', function () {
-    let pd = new PlayerData(1, 0)
-    assert.equal(pd.id, 1);
-    assert.equal(pd.type, 0);
-  });
+describe('PlayerData', () => {
+  describe('.constructor()', () => {
+    it('正しく初期化できる', function () {
+      let pd = new PlayerData(1, 0)
+      assert.equal(pd.ID, 1)
+      assert.equal(pd.Type, 0)
+    })
+  })
 
-  it('IDがおかしい場合はエラーになる', function () {
-    assert.throws(() => {new PlayerData('invalid ID', 0)});
-  });
+  describe('.id()', () => {
+    it('IDを更新できる', () => {
+      let pd = new PlayerData(2, 1)
+      assert.equal(pd.ID, 2)
 
-  it('タイプがおかしい場合はエラーになる', function () {
-    assert.throws(() => {new PlayerData(1, 'Invalid Type')});
-  });
-});
+      pd = pd.id(3)
+      assert.equal(pd.ID, 3)
+      assert.equal(pd.constructor.name, 'PlayerData')
+    })
+
+    it('IDがおかしい場合はエラーになる', () => {
+      assert.throws(() => {new PlayerData('invalid ID', 0)})
+    })
+  })
+
+  describe('.type()', () => {
+    it('タイプを更新できる', () => {
+      let pd = new PlayerData(3, 0)
+      assert.equal(pd.Type, 0)
+
+      pd = pd.type(1)
+      assert.equal(pd.Type, 1)
+      assert.equal(pd.constructor.name, 'PlayerData')
+    })
+
+    it('タイプがおかしい場合はエラーになる', () => {
+      assert.throws(() => {new PlayerData(1, 'Invalid Type')})
+    })
+  })
+})
