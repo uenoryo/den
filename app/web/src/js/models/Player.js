971024fca0ab3b78497e9ab5080b319e7b8bf7b6
uenoryo <r.r.ag0611@outlook.jp>
2018-06-17 12:56:48 +0900

    Implement Player.draw().

--- app/web/src/js/models/Player.js
+++ app/web/src/js/models/Player.js
@@ -1,4 +1,4 @@
-import cst from '@/constants'
+import HandData from '../data/HandData'
 
 export default class Player {
   constructor(data) {
@@ -6,5 +6,21 @@ export default class Player {
       throw new Error('Invalid PlayerData')
     }
     this.data = data
+    this.hand = new HandData([])
+  }
+
+  draw(deck) {
+    if (deck.cardNum() === 0) {
+      return
+    }
+    this.addCardToHand(deck.turn())
+  }
+
+  addCardToHand(card) {
+    if (card.constructor.name !== 'CardData') {
+      throw new Error(`Invalid Card will add to hand [${card.constructor.name}]`)
+    }
+
+    this.hand.Cards.push(card)
   }
 }
