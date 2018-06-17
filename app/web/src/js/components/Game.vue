fb8ab001c0bdf306198c0aad89e2141061ff8eb9
uenoryo <r.r.ag0611@outlook.jp>
2018-06-17 15:19:29 +0900

    Display players hand and deck info.

--- app/web/src/js/components/Game.vue
+++ app/web/src/js/components/Game.vue
@@ -1,12 +1,26 @@
 <template>
   <div class='game'>
-    <h1>{{ message }}</h1>
+    <div class='hands'>
+      <h1>手札一覧</h1>
+      <br>
+      <div v-for='player in players'>
+        <h2>プレイヤーID: {{ player.data.ID }}</h2>
+        <span v-for='card in player.hand.Cards'>
+          [{{ card.toString() }}]
+        </span>
+        <br>
+        <br>
+      </div>
+    </div>
+    <div class='field'>
+      <h1>デッキ</h1>
+      <div>残り: {{ dealer.deck.cardNum() }}枚</div>
+    </div>
   </div>
 </template>
 
 <script>
 import God from '@/models/God'
-import Config from '@/config'
 import Constants from '@/constants'
 
 export default {
@@ -15,17 +29,34 @@ export default {
     return {
       message: 'Play',
       god: null,
+      dealer: null,
       players: null,
     }
   },
   mounted () {
-    this.god = new God(Config)
+    this.god = new God
     this.setup()
   },
   methods: {
     setup () {
+      let deck = this.god.createDeck()
+      // Create dealer
+      this.dealer = this.god.createDealer(deck)
+
       // Create entry player
       this.players = this.god.createPlayers()
+
+      this.dealer.shuffle()
+
+      // Deal cards to players
+      for (let i = 0; i < Constants.PlayerHandStartAmount; i++) {
+        this.dealCardToPlayers()
+      }
+    },
+    dealCardToPlayers () {
+      for (let idx in this.players) {
+        this.dealer.deal(this.players[idx])
+      }
     },
   }
 }
