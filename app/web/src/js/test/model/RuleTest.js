04549bf10ca8fa6711073506b7bbdca38dc51b97
uenoryo <r.r.ag0611@outlook.jp>
2018-06-17 19:19:28 +0900

    RuleTest 追加.

--- app/web/src/js/test/model/RuleTest.js
+++ app/web/src/js/test/model/RuleTest.js
@@ -2,6 +2,7 @@ import assert from 'assert'
 import Constants from '../../constants'
 import Rule from '../../models/Rule'
 import CardData from '../../data/CardData'
+import HandData from '../../data/HandData'
 
 describe('Rule', () => {
   describe('.canPut()', () => {
@@ -48,5 +49,39 @@ describe('Rule', () => {
         assert.equal(Rule.canPut(tests[idx].input.field, tests[idx].input.target), tests[idx].output)
       })
     }
+  })
+
+  describe('.isPank()', () => {
+    it(`${Constants.PlayerHandMaxAmount}枚以下はパンクにならない`, () => {
+      let cards = [
+          new CardData(Constants.CardMarkClub, 10),
+          new CardData(Constants.CardMarkClub, 11),
+      ]
+      assert.equal(Rule.isPank(new HandData(cards)), false)
+    })
+
+    it(`${Constants.PlayerHandMaxAmount+1}枚以上はパンクになる`, () => {
+      let cards = [
+          new CardData(Constants.CardMarkClub, 1),
+          new CardData(Constants.CardMarkClub, 2),
+          new CardData(Constants.CardMarkClub, 3),
+          new CardData(Constants.CardMarkClub, 4),
+          new CardData(Constants.CardMarkClub, 5),
+          new CardData(Constants.CardMarkClub, 6),
+          new CardData(Constants.CardMarkClub, 7),
+          new CardData(Constants.CardMarkClub, 8),
+          new CardData(Constants.CardMarkClub, 9),
+          new CardData(Constants.CardMarkClub, 10),
+          new CardData(Constants.CardMarkClub, 11),
+          new CardData(Constants.CardMarkClub, 12),
+          new CardData(Constants.CardMarkClub, 13),
+          new CardData(Constants.CardMarkDiamond, 1),
+      ]
+      assert.equal(Rule.isPank(new HandData(cards)), true)
+    })
+
+    it(`HandDataを渡さないとエラーになる`, () => {
+      assert.throws(() => {Rule.isPank('invalid hand')})
+    })
   })
 })
