b48373c6a50d9476a3f6a916c9d992d3ee4b9a34
uenoryo <r.r.ag0611@outlook.jp>
2018-06-17 12:15:56 +0900

    Implement Deck model.

--- app/web/src/js/models/Deck.js
+++ app/web/src/js/models/Deck.js
@@ -1,10 +1,19 @@
-import cst from '../constants'
-
 export default class Deck {
   constructor(data) {
     if (data.constructor.name !==  'DeckData') {
       throw new Error('Invalid DeckData')
     }
     this.data = data
+  }
+
+  cardNum() {
+    return this.data.Cards.length
+  }
+
+  turn() {
+    if (this.cardNum() === 0) {
+      throw new Error('Cant trun Empty deck')
+    }
+    return this.data.Cards.shift()
   }
 }
