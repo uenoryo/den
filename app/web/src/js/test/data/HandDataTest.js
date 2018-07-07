1c72c1bdbb73432f26c74cce4c3809d3732e3330
uenoryo <r.r.ag0611@outlook.jp>
2018-07-07 23:09:14 +0900

    HandDataにセッター、ゲッター、Hand追加.

--- app/web/src/js/test/data/HandDataTest.js
+++ app/web/src/js/test/data/HandDataTest.js
@@ -1,10 +1,11 @@
 import assert from 'assert'
 import CardData from '../../data/CardData'
 import HandData from '../../data/HandData'
+import Constants from '../../constants'
 
-describe('HandData', () => {
-  describe('.constructor()', () => {
-    it('正しく初期化できる', () => {
+describe ('HandData', () => {
+  describe ('.constructor()', () => {
+    it ('正しく初期化できる', () => {
       let cards = [
           new CardData(0, 10),
           new CardData(1, 11),
@@ -17,13 +18,15 @@ describe('HandData', () => {
       assert.equal(dd.Cards[1].Num, 11)
     })
 
-    it('カードが空でも正しく初期化できる', () => {
+    it ('カードが空でも正しく初期化できる', () => {
       new HandData([])
     })
+
+    it (`最大値が${Constants.PlayerHandMaxAmount}枚で手札が形成されている`)
   })
 
-  describe('.cards()', () => {
-    it('カードを更新できる', () => {
+  describe ('.cards()', () => {
+    it ('カードを更新できる', () => {
       let cards = [
           new CardData(2, 12),
       ]
@@ -41,9 +44,17 @@ describe('HandData', () => {
       assert.equal(dd.Cards[0].Num, 13)
     })
 
-    it('カード以外を入れようとした場合はエラーになる', () => {
+    it ('カード以外を入れようとした場合はエラーになる', () => {
       let cards = ['dummy card']
       assert.throws(() => {new HandData(cards)})
     })
+  })
+
+  describe ('.set()', () => {
+    it ('カードをセットできる')
+  })
+
+  describe ('get()', () => {
+    it ('指定したインデックスのカードを取得できる')
   })
 })
