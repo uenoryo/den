31f5e5ee23be8abc64def2715d82572e76c8ebca
uenoryo <r.r.ag0611@outlook.jp>
2018-07-07 23:28:12 +0900

    HandTest 追加.

--- app/web/src/js/test/data/HandDataTest.js
+++ app/web/src/js/test/data/HandDataTest.js
@@ -11,18 +11,39 @@ describe ('HandData', () => {
           new CardData(1, 11),
       ]
 
-      let dd = new HandData(cards)
-      assert.equal(dd.Cards[0].Mark, 0)
-      assert.equal(dd.Cards[0].Num, 10)
-      assert.equal(dd.Cards[1].Mark, 1)
-      assert.equal(dd.Cards[1].Num, 11)
+      let hd = new HandData(cards)
+      assert.equal(hd.Cards[0].Mark, 0)
+      assert.equal(hd.Cards[0].Num, 10)
+      assert.equal(hd.Cards[1].Mark, 1)
+      assert.equal(hd.Cards[1].Num, 11)
     })
 
     it ('カードが空でも正しく初期化できる', () => {
       new HandData([])
     })
 
-    it (`最大値が${Constants.PlayerHandMaxAmount}枚で手札が形成されている`)
+    describe (`手札が形成されている`, () => {
+      let cards = [
+          new CardData(2, 5),
+          new CardData(1, 11),
+      ]
+      let hd = new HandData(cards)
+
+      it (`枚数が${Constants.PlayerHandMaxAmount}枚である`, () => {
+        assert.equal(hd.Hand.length, Constants.PlayerHandMaxAmount)
+      })
+
+      it ('手札にカードが入っている', () => {
+        assert.equal(hd.Hand[0].Mark, 2)
+        assert.equal(hd.Hand[0].Num, 5)
+        assert.equal(hd.Hand[1].Mark, 1)
+        assert.equal(hd.Hand[1].Num, 11)
+      })
+
+      it ('範囲外はnullである', () => {
+        assert.equal(hd.Hand[Constants.PlayerHandMaxAmount], null)
+      })
+    })
   })
 
   describe ('.cards()', () => {
