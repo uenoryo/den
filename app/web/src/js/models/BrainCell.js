7635def25ae7cccb253c2eb1431f5d6693478149
uenoryo <r.r.ag0611@outlook.jp>
2018-07-23 00:00:35 +0900

    ３枚揃っているカードは優先度を下げる.

--- app/web/src/js/models/BrainCell.js
+++ app/web/src/js/models/BrainCell.js
@@ -2,15 +2,26 @@ import Constants from '../constants'
 import Rule from './Rule'
 
 export default class BrainCell {
+  // 素上がりできる場合におけるDrawの優先度を操作します
   static canDoneButDraw (data, value, rate) {
-    if (value == null) {
-      value = 10
-    }
     if (data.SelfHand.Cards.length !== 1) {
       return
     }
     if (Object.keys(data.PuttableIdx).length === 2 && BrainCell.hit(rate)) {
       data.PuttableIdx['-1'] += value
+    }
+  }
+
+  // 同じ数字のカードが3枚揃っている場合、そのカードの優先度を操作します
+  static waitAnko (data, value, rate) {
+    for (let idx in data.PuttableIdx) {
+      if (idx === '-1') {
+        continue
+      }
+      let card = data.SelfHand.Cards[idx]
+      if (data.SelfHand.numAmount(card.Num) === 3) {
+        data.PuttableIdx[idx] -= value
+      }
     }
   }
 
--- app/web/src/js/models/brains/Basic.js
+++ app/web/src/js/models/brains/Basic.js
@@ -3,7 +3,8 @@ import Cell from '../BrainCell'
 
 export default class Basic extends Brain {
   putOrDraw () {
-    Cell.canDoneButDraw(this.data)
+    Cell.canDoneButDraw(this.data, 10)
+    Cell.waitAnko(this.data, 5)
   }
 
   den () {
