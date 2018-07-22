a72d87536c6a28ec97e3f47983f99ab6bbf2a89e
uenoryo <r.r.ag0611@outlook.jp>
2018-07-22 12:19:02 +0900

    BrainDataTest更新.

--- app/web/src/js/constants/index.js
+++ app/web/src/js/constants/index.js
@@ -64,9 +64,14 @@
   ActionTypePut: 2,
   ActionTypeForceDraw: 3,
 
+  // GameSetType
   GameSetTypePlainDone: 1,
   GameSetTypePank: 2,
   GameSetTypeDen: 3,
   GameSetTypeAnko: 4,
   GameSetTypeChitoi: 5,
+
+  // Plan
+  PlanTargetPlainDone: 1,
+  PlanTargetDenAndPlainDone: 2,
 }
