95226811d51c25356643c44a5438c8af07b9be9c
uenoryo <r.r.ag0611@outlook.jp>
2018-08-01 22:56:43 +0900

    PlayerのメソッドをいくつかHandDataに移動.

--- app/web/src/js/data/HandData.ts
+++ app/web/src/js/data/HandData.ts
@@ -1,4 +1,5 @@
 import { CardNum } from '../type/Type'
+import { CardNums } from '../constant/Card'
 import CardData from './CardData'
 
 export default class DeckData {
@@ -36,5 +37,38 @@ export default class DeckData {
       }
     }
     return amount
+  }
+
+  numTotal(): number {
+    let sum = 0
+    for (let card of this.Cards) {
+      sum += card.Num
+    }
+    return sum
+  }
+
+  lonelyNumForChitoi(): CardNum | null {
+    let num: CardNum | null = null
+    for (let card of this.Cards) {
+      if (this.numAmount(card.Num) % 2 === 1) {
+        // 枚数が奇数の数字が2種類以上ある場合は該当しない
+        if (num !== null) {
+          return null
+        }
+        num = card.Num
+      }
+    }
+    return num
+  }
+
+  pairCount(): number {
+    let count = 0
+    for (let num of CardNums) {
+      let amount = this.numAmount(num)
+      if (amount >= 2) {
+        count += Math.floor(amount / 2)
+      }
+    }
+    return count
   }
 }
--- app/web/src/js/test/data/HandDataTest.ts
+++ app/web/src/js/test/data/HandDataTest.ts
@@ -15,7 +15,7 @@ describe('HandData', () => {
       assert.equal(dd.Cards[0].Num, 1)
     })
     describe ('手札のコストの合計を返すことができる', () => {
-      it ('手札が少ないケース', () => {
+      it('手札が少ないケース', () => {
         let cards = [
             new CardData(0, 1),
             new CardData(0, 13),
@@ -24,12 +24,12 @@ describe('HandData', () => {
         assert.equal(hd.Cost, 2)
       })
 
-      it ('手札がないケース', () => {
+      it('手札がないケース', () => {
         let hd = new HandData([])
         assert.equal(hd.Cost, 0)
       })
 
-      it ('手札が多いケース', () => {
+      it('手札が多いケース', () => {
         let cards = [
             new CardData(0, 0), // 5
             new CardData(0, 1), // 1
@@ -47,35 +47,6 @@ describe('HandData', () => {
     })
   })
 
-  describe ('.aggregate()', () => {
-    describe ('カードの数字毎に枚数を集計したオブジェクトを返すことができる', () => {
-      let cards = [
-          new CardData(0, 1),
-          new CardData(0, 13),
-          new CardData(1, 13),
-          new CardData(2, 13),
-          new CardData(3, 13),
-          new CardData(4, 0),
-          new CardData(5, 0),
-      ]
-      let hd = new HandData(cards)
-      let res = hd.aggregate()
-
-      it ('A は 1枚', () => {
-        assert.equal(res[1], 1)
-      })
-      it ('JOKER は 2枚', () => {
-        assert.equal(res[0], 2)
-      })
-      it ('K は 4枚', () => {
-        assert.equal(res[13], 4)
-      })
-      it ('それ以外は 0枚', () => {
-        assert.equal(res[2], 0)
-      })
-    })
-  })
-
   describe ('.numAmount()', () => {
     describe ('カードの数字毎に枚数を集計したオブジェクトを返すことができる', () => {
       let cards = [
@@ -89,17 +60,101 @@ describe('HandData', () => {
       ]
       let hd = new HandData(cards)
 
-      it ('A は 1枚', () => {
+      it('A は 1枚', () => {
         assert.equal(hd.numAmount(1), 1)
       })
-      it ('JOKER は 2枚', () => {
+      it('JOKER は 2枚', () => {
         assert.equal(hd.numAmount(0), 2)
       })
-      it ('K は 4枚', () => {
+      it('K は 4枚', () => {
         assert.equal(hd.numAmount(13), 4)
       })
-      it ('6 は 0枚', () => {
+      it('6 は 0枚', () => {
         assert.equal(hd.numAmount(6), 0)
+      })
+    })
+  })
+
+  describe('.pairCount()', () => {
+    describe ('手札に含まれているペアの数を返すことができる', () => {
+      it('ペアなし', () => {
+        let hand = new HandData([
+          new CardData(0, 1),
+          new CardData(0, 2),
+          new CardData(0, 3),
+        ])
+        assert.equal(hand.pairCount(), 0)
+      })
+      it('1ペア', () => {
+        let hand = new HandData([
+          new CardData(0, 1),
+          new CardData(0, 2),
+          new CardData(1, 2),
+        ])
+        assert.equal(hand.pairCount(), 1)
+      })
+      it('2ペア', () => {
+        let hand = new HandData([
+          new CardData(0, 4),
+          new CardData(0, 5),
+          new CardData(1, 5),
+          new CardData(0, 6),
+          new CardData(1, 6),
+          new CardData(2, 6),
+        ])
+        assert.equal(hand.pairCount(), 2)
+      })
+      it('3ペア (同じカード4枚も含む)', () => {
+        let hand = new HandData([
+          new CardData(3, 11),
+          new CardData(0, 9),
+          new CardData(1, 9),
+          new CardData(2, 9),
+          new CardData(3, 9),
+          new CardData(4, 0),
+          new CardData(5, 0),
+          new CardData(0, 13),
+        ])
+        assert.equal(hand.pairCount(), 3)
+      })
+    })
+  })
+
+  describe('.lonelyNumForChitoi()', () => {
+    describe ('ペアがない1種類のカードを返すことができる', () => {
+      it('該当しない (ペア以外が複数ある)', () => {
+        let deck = new HandData([
+          new CardData(0, 1),
+          new CardData(1, 1),
+          new CardData(0, 2),
+          new CardData(0, 3),
+        ])
+        assert.equal(deck.lonelyNumForChitoi(), null)
+      })
+      it('該当しない (ペアのみ)', () => {
+        let deck = new HandData([
+          new CardData(0, 4),
+          new CardData(1, 4),
+          new CardData(0, 5),
+          new CardData(2, 5),
+        ])
+        assert.equal(deck.lonelyNumForChitoi(), null)
+      })
+      it('該当する (1枚単体)', () => {
+        let deck = new HandData([
+          new CardData(0, 5)
+        ])
+        assert.equal(deck.lonelyNumForChitoi(), 5)
+      })
+      it('該当する (複数ペア)', () => {
+        let deck = new HandData([
+          new CardData(0, 8),
+          new CardData(1, 8),
+          new CardData(0, 9),
+          new CardData(1, 9),
+          new CardData(2, 12),
+        ])
+        assert.equal(deck.lonelyNumForChitoi(), 12)
       })
     })
   })
