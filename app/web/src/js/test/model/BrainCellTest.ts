340d0efe4a2442eced7110e5f71b58c631412005
uenoryo <r.r.ag0611@outlook.jp>
2018-08-05 11:39:52 +0900

    BrainCellテスト項目追加.

--- app/web/src/js/test/model/BrainCellTest.ts
+++ app/web/src/js/test/model/BrainCellTest.ts
@@ -30,7 +30,7 @@ describe('BrainCell', () => {
           ],
           priorities: [
             [-1, 0],
-            [0, 10],
+            [0,  0],
           ],
           drawPriority: 10,
         },
@@ -52,8 +52,8 @@ describe('BrainCell', () => {
           ],
           priorities: [
             [-1, 0],
-            [0, 10],
-            [1, 10],
+            [0,  0],
+            [1,  0],
           ],
           drawPriority: 0,
         },
@@ -68,6 +68,70 @@ describe('BrainCell', () => {
           assert.equal(data.getPriority(DrawActionID), t.drawPriority)
         })
       }
+    })
+  })
+
+  describe('.waitAnko()', () => {
+    context('Ankoができる場合に該当するカードの優先度を操作できる', () => {
+      type test = {
+        title: string,
+        hand: CardData[]
+        priorities: [number, number][],
+        drawPriority: number,
+      }
+
+      let tests: test[] = [
+        {
+          title: 'Ankoできるパターン',
+          hand: [
+            new CardData(1, 10),
+            new CardData(2, 10),
+            new CardData(3, 10),
+            new CardData(1, 13),
+          ],
+          priorities: [
+            [-1, 0],
+            [0,  0],
+          ],
+          drawPriority: 10,
+        },
+        {
+          title: 'Ankoできないパターン',
+          hand: [
+            new CardData(1, 10),
+            new CardData(2, 10),
+            new CardData(1, 13),
+          ],
+          priorities: [
+            [-1, 0],
+            [0,  0],
+          ],
+          drawPriority: 0,
+        },
+      ]
+      for (let t of tests) {
+        it(t.title, () => {
+          let data = new BrainData
+          data.FieldCard = new CardData(0, 10)
+          data.SelfHand = new HandData(t.hand)
+          data.HandPriorities = t.priorities
+          let bc = new BrainCell
+          bc.waitAnko(data, 10, 100)
+          assert.equal(data.getPriority(0), t.drawPriority)
+        })
+      }
+    })
+  })
+
+  describe('.hit()', () => {
+    context('確率で真偽を変えす', () => {
+      let b = new BrainCell
+      it('100では必ずtrue', () => {
+        assert.equal(b.hit(100), true)
+      })
+      it('0では必ずfalse', () => {
+        assert.equal(b.hit(0), false)
+      })
     })
   })
 })
