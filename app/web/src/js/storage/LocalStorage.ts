b55f28d747c1c644c02cfd3a547275bb475937a2
uenoryo <r.r.ag0611@outlook.jp>
2018-08-12 16:26:00 +0900

    LocalStorage実装.

--- app/web/src/js/storage/LocalStorage.ts
+++ app/web/src/js/storage/LocalStorage.ts
@@ -2,15 +2,21 @@ import Storager from './Storager'
 import ScoreData from '../data/ScoreData'
 
 export default class LocalStorage implements Storager {
+  private scoreKey: string = 'den.Score'
+
   saveScore(score: ScoreData[]): void {
-    //
+    localStorage.setItem(this.scoreKey, JSON.stringify(score))
   }
 
   getScore(): ScoreData[] | null {
-    return null
+    let item = localStorage.getItem(this.scoreKey)
+    if (item === null) {
+      return null
+    }
+    return JSON.parse(item) as ScoreData[]
   }
 
   clearScore(): void {
-    //
+    localStorage.removeItem(this.scoreKey)
   }
 }
