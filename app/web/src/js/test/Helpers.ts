6f34d8f56c00f85eb82c38444a3c6ab5665ca6d3
uenoryo <r.r.ag0611@outlook.jp>
2018-08-12 11:05:25 +0900

    テスト用のヘルパ追加.

---
+++ app/web/src/js/test/Helpers.ts
@@ -0,0 +1,13 @@
+import PlayerData from '../data/PlayerData'
+import Brain from '../model/Brain'
+import Player from '../model/Player'
+import Players from '../model/Players'
+
+export function testPlayers() {
+  return new Players(
+    new Player(new PlayerData(1, 0), new Brain),
+    new Player(new PlayerData(2, 0), new Brain),
+    new Player(new PlayerData(3, 0), new Brain),
+    new Player(new PlayerData(4, 0), new Brain)
+  )
+}
