48b14a161c9523f1117799230c6952c53143d704
uenoryo <r.r.ag0611@outlook.jp>
2018-08-19 18:38:07 +0900

    remove old scripts.

--- app/web/src/scripts/env/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  DEBUG: false,
-  DEBUG_DECK_ID: 1,
-  Player2Brain: 'Basic',
-  Player3Brain: 'Basic',
-  Player4Brain: 'Basic',
-}
--- app/web/src/scripts/models/brains/FastDone.js
+++
@@ -1,7 +0,0 @@
-import Brain from '../Brain'
-
-export default class FastDone extends Brain {
-  den () {
-    return false
-  }
-}
