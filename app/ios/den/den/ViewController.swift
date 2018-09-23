535dc3f6ea255331e0ce10b8a988a9959673db6e
uenoryo <r.r.ag0611@outlook.jp>
2018-09-23 10:59:02 +0900

    [ios] Implement webview.

--- app/ios/den/den/ViewController.swift
+++ app/ios/den/den/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
 
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = URL(string: "http://den.uma-b.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
