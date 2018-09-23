4ecfb4c86136db11df4a8e3d778966df717f555f
uenoryo <r.r.ag0611@outlook.jp>
2018-09-23 09:30:54 +0900

    Add ios project.

---
+++ app/ios/den/denTests/denTests.swift
@@ -0,0 +1,36 @@
+//
+//  denTests.swift
+//  denTests
+//
+//  Created by uenoryo on 2018/09/23.
+//  Copyright © 2018年 uenoryo. All rights reserved.
+//
+
+import XCTest
+@testable import den
+
+class denTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
