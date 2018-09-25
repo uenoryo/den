7b2549292b92a911da5754725076dd3ee7d3ff29
uenoryo <r.r.ag0611@outlook.jp>
2018-09-25 23:51:08 +0900

    設定を保存可能にする.

--- app/web/src/js/storage/Storager.ts
+++ app/web/src/js/storage/Storager.ts
@@ -1,7 +1,10 @@
 import ScoreData from '../data/ScoreData'
+import { PreferenceKey } from '../type/Type'
 
 export default interface Storager {
   saveScore(score: ScoreData[]): void
   getScore(): ScoreData[] | null
   clearScore(): void
+  savePreference(key: PreferenceKey, value: number): void
+  getPreference(key: PreferenceKey): number
 }
