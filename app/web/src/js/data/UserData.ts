d3334c3e089781554997cbb002a1e2967baa3cb3
uenoryo <r.r.ag0611@outlook.jp>
2018-10-14 16:57:14 +0900

    金額を数字にする

--- app/web/src/js/data/UserData.ts
+++ app/web/src/js/data/UserData.ts
@@ -12,4 +12,27 @@ export default class UserData {
     this.Money = 0
     this.Stamina = 0
   }
+
+  get MoneyString(): string {
+    const unitJP = ["万", "億", "兆", "京", "垓", "秭"]
+    let str = String(this.Money)
+
+    let index = 0
+    let res = ""
+    while (0 < str.length) {
+      let num = parseInt(str.slice(-1*4)) // 一度数字にすることで 021 => 21 のように0を切る
+      if (num !== 0) {
+        let tmp = String(num) + unitJP[index]
+        res = tmp + res
+      }
+      str = str.slice(0, -1*4) // 下4桁削除
+      index++
+    }
+
+    if (res === "") {
+      res = "0"
+    }
+
+    return res + "円"
+  }
 }
