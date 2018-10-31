61b298c755684a3f1077d3be6c49390df54dbe88
uenoryo <r.r.ag0611@outlook.jp>
2018-10-31 23:37:26 +0900

    ビジネス購入時にランクを同期.

--- app/web/src/js/data/UserData.ts
+++ app/web/src/js/data/UserData.ts
@@ -5,6 +5,7 @@ export default class UserData {
   public Token: string
   public Name: string
   public Money: number
+  public Rank: number
   public Stamina: number
   public BestScore: number
   public BestTotalScore: number
@@ -14,6 +15,7 @@ export default class UserData {
     this.Token = ""
     this.Name = ""
     this.Money = 0
+    this.Rank = 0
     this.Stamina = 0
     this.BestScore = 0
     this.BestTotalScore = 0
