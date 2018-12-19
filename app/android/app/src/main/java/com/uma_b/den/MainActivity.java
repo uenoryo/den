711c4cdba668bb88c5788f220c1de1dce5cec372
uenoryo <r.r.ag0611@outlook.jp>
2018-12-19 22:23:52 +0900

    implement android webview.

--- app/android/app/src/main/java/com/uma_b/den/MainActivity.java
+++ app/android/app/src/main/java/com/uma_b/den/MainActivity.java
@@ -2,6 +2,8 @@ package com.uma_b.den;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setDomStorageEnabled(true);
+        wv.loadUrl("https://den.uma-b.com/");
     }
 }
